cc1f55ffa34d7ec6dd4a61d9b94adb3fedaedfdb
xtophe02 <moreira.christophe@outlook.com>
2025-01-03 14:19:10 +0000

    main test

---
+++ main.py
@@ -0,0 +1,12 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+
+def main():
+    print("Starting asteroids!")
+
+
+if __name__ == "__main__":
+    main()
