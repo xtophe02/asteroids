dabc5657b36204015552a1dc61c722bf11acd799
xtophe02 <moreira.christophe@outlook.com>
2025-01-03 15:59:13 +0000

    added pygame window

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    frames_per_second = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = frames_per_second.tick(60)
+        print(f"dt: {dt}")
 
 
 if __name__ == "__main__":
